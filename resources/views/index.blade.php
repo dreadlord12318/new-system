6dfcb71e187af85d66cd6fbe338041573067748c
= <dreadlord12318@gmail.com>
2021-10-15 15:30:46 +0800

    Responsive Navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,66 +10,40 @@
       </svg>
 
       <div>
-        <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
-          <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-            <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-              <div class="flex items-center justify-between w-full md:w-auto">
-                <a href="/">
-                  <span class="sr-only">Workflow</span>
-                  <img class="h-8 w-auto sm:h-10" src="images/WCTT-Site-Image-NavLogo.png">
-                </a>
-                <div class="-mr-2 flex items-center md:hidden">
-                  <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <!-- Heroicon name: outline/menu -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                  </button>
-                </div>
-              </div>
+      <nav class = "bg-white shadow mb-10">
+    <div x-data="{ isOpen: false }" class="md:max-w-7xl lg:max-w-5xl mx-auto py-6 px-6 md:px-0 md:flex md:justify-between md:items-center">
+        <div class="flex justify-between items-center">
+            <div class="flex items-center md:ml-4">
+            <a href="/">
+            <span class="sr-only">Westcott Dyson</span>
+            <img class="h-8 w-auto sm:h-10" src="/images/WCTT-Site-Image-NavLogo.png" alt="">
+          </a>
             </div>
-            <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-
-              <a href="/contact" class="font-medium text-gray-900 hover:text-gray-900">Contact Us</a>
-
-            </div>
-          </nav>
-        </div>
-
-        <!--
-          Mobile menu, show/hide based on menu open state.
-
-          Entering: "duration-150 ease-out"
-            From: "opacity-0 scale-95"
-            To: "opacity-100 scale-100"
-          Leaving: "duration-100 ease-in"
-            From: "opacity-100 scale-100"
-            To: "opacity-0 scale-95"
-        -->
-        <div class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-          <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-            <div class="px-5 pt-4 flex items-center justify-between">
-              <div>
-              <img class="h-8 w-auto" src="/images/WCTT-Site-Image-NavLogo.png" alt="">
-              </div>
-              <div class="-mr-2">
-                <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                  <span class="sr-only">Close main menu</span>
-                  <!-- Heroicon name: outline/x -->
-                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                  </svg>
+            <div class="flex md:hidden">
+                <button type="button"
+                 class="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-grape-500"
+                 aria-label="toggle menu"
+                 x-on:click="isOpen = !isOpen">
+                <span class="sr-only">Open main menu</span>
+                <!-- Heroicon name: outline/menu -->
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
                 </button>
-              </div>
             </div>
-            <div class="px-2 pt-2 pb-3 space-y-1">
-            
-
-<a href="/contact" class="font-medium text-gray-900 hover:text-gray-900">Contact Us</a>
-            </div>
-          </div>
+            <div class="hidden md:block md:ml-4 space-x-10 lg:flex lg:ml-4">
+              <a href="/contact" class="font-medium text-gray-900 hover:text-gray-900">Contact Us</a>
         </div>
+        </div>
+        <!-- Menu Mobile -->
+        <div x-show="isOpen" class="md:flex items-center">
+            <div class="flex flex-wrap justify-around content-around md:hidden flex-col md:flex-row md:ml-6">
+            <a href="/contact" class="font-medium text-gray-900 hover:text-gray-900 mt-4">Contact Us</a>
+            </div>
+        </div>
+    </div>
+
+</nav>
       </div>
 
       <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -3,72 +3,7 @@
 @section('content')
 
 <div class="min-h-screen bg-white">
-  <header class="relative z-10">
-    <div class="bg-gray-50">
-      <nav class="relative max-w-7xl mx-auto flex items-center justify-between pt-8 px-6 xl:px-8" aria-label="Global">
-        <div class="flex items-center justify-between w-full lg:w-auto">
-          <a href="/">
-            <span class="sr-only">Workflow</span>
-            <img class="h-8 w-auto sm:h-10" src="/images/WCTT-Site-Image-NavLogo.png" alt="">
-          </a>
-          <div class="-mr-2 flex items-center lg:hidden">
-            <button type="button" class="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-grape-500" aria-expanded="false">
-              <span class="sr-only">Open main menu</span>
-              <!-- Heroicon name: outline/menu -->
-              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            </button>
-          </div>
-          <div class="hidden space-x-10 lg:flex lg:ml-10">
-              <a href="/contact" class="font-medium text-gray-900 hover:text-gray-900">Contact Us</a>
-        </div>
-        </div>
-       
-        <div class="hidden lg:flex lg:items-center lg:space-x-6">
-         
-        </div>
-      </nav>
-    </div>
-
-    <!--
-      Mobile menu, show/hide based on menu open state.
-
-      Entering: "duration-150 ease-out"
-        From: "opacity-0 scale-95"
-        To: "opacity-100 scale-100"
-      Leaving: "duration-100 ease-in"
-        From: "opacity-100 scale-100"
-        To: "opacity-0 scale-95"
-    -->
-    <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top lg:hidden">
-      <div class="rounded-lg shadow-lg bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-        <div class="px-5 pt-4 flex items-center justify-between">
-          <div>
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=purple&shade=600" alt="">
-          </div>
-          <div class="-mr-2">
-            <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-grape-500">
-              <span class="sr-only">Close menu</span>
-              <!-- Heroicon name: outline/x -->
-              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-          </div>
-        </div>
-        <div class="pt-5 pb-6">
-          <div class="px-2 space-y-1">
-          
-
-<a href="/contact" class="font-medium text-gray-900 hover:text-gray-900">Contact Us</a>
-          </div>
-         
-        </div>
-      </div>
-    </div>
-  </header>
-
+  @include('header')
 
 
   <div class="py-16 xl:py-36 px-4 sm:px-6 lg:px-8 bg-white overflow-hidden">
