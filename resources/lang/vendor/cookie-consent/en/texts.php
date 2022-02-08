6ae2c325065e73269a0bbb8de3d29efe6730fcd0
= <dreadlord12318@gmail.com>
2022-02-08 08:45:28 +0800

    GDPR Copy update

--- resources/lang/vendor/cookie-consent/en/texts.php
+++ resources/lang/vendor/cookie-consent/en/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => 'Our website uses cookies to give you the best and most relevant experience. By clickin on allow, you give consent to the use of cookies as per our <a href = "/privacy">privacy policy</a>',
+    'message' => 'Our website uses cookies to give you the best browsing experience. By selecting on allow, you consent to the use of cookies. For more information, please see our <a href = "/privacy">privacy policy</a>.',
     'agree' => 'Allow cookies',
 ];
